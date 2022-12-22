82175365b8e49538beba34757fb2c9f8ce3839c4
Documenter.jl <documenter@juliadocs.github.io>
2022-12-22 02:53:48 +0000

    build based on 995cb09

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DifferentiableOSQP","category":"page"},{"location":"#DifferentiableOSQP","page":"Home","title":"DifferentiableOSQP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DifferentiableOSQP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DifferentiableOSQP]","category":"page"}]
+}
